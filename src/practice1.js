678df408cbc0e8510b57ccd49c0bf78175149cf4
Jiancheng LI <jiancheng.li@thoughtworks.com>
2020-06-30 21:35:53 +0800

    feat, impl parseData and inject functions

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,11 @@
-const parseData
+const parseData = ( input ) => {
+    return input.data.map( row => {
+        let output = {};
+        input.column.forEach((columnData, columnIndex) => {
+            output[columnData.name] = row[columnIndex];
+        });
+        return output;
+    });
+}
+
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,16 @@
-const inject
+Array.prototype.insert = function( index, content ) {
+    this.splice( index, 0, content );
+};
+
+const desc = (sections) => {
+    return sections.sort((a,b) => b.index - a.index);
+}
+
+const inject = (items, sections) => {
+    let cloneItems = [...items];
+    desc(sections).forEach( section => {
+        cloneItems.insert( section.index, section.content );
+    });
+    return cloneItems;
+}
 export { inject };
